275edaf76523751b23e3d5f57ba2ffec070da8eb
Lu69as <lukasokken@gmail.com>
2024-06-09 15:05:01 +0200

    A bit of simplifying and bug fixing

--- main.js
+++ main.js
@@ -1,10 +1,4 @@
 let d = new Date();
-const user = {
-  name: 'Lukas Okkenhaug',
-  desc: ' A Creative Fella from Norway',
-  age: (d.getDate === 15 && d.getMonth() === 7) ? `Its my Birthday! I'm turning ${(d.getFullYear() - 2006)} Years old` 
-    : `I am ${(d.getFullYear() - (d.getMonth() <= 7 ? 2007 : 2006))} Years old`
-}
 
 let currentbg = Math.floor(Math.random() * 5 + 1);
 function changebackgrund() {
@@ -12,8 +6,7 @@ function changebackgrund() {
     document.querySelector('.App-header').classList.remove('spatter' + i)
   document.querySelector('.App-header').classList.add('spatter' + currentbg++)
 
-  if (currentbg > 5)
-    currentbg = 1;
+  if (currentbg > 5) currentbg = 1;
 }
 
 function scroll() {
@@ -22,62 +15,36 @@ function scroll() {
   var scrolled = (winScroll / height) * 100;
   document.querySelector(".scrollbar p").style.top = scrolled - 10 + "%";
 
-  document.querySelector(".App-header").style.backgroundSize = 100 + scrolled * 5 + "%"
-
-  document.querySelectorAll(".transition:nth-child(even)").forEach((e) => {
-    e.style.left = -135 + scrolled + "%";
-  })
-  document.querySelectorAll(".transition:nth-child(odd)").forEach((e) => {
-    e.style.right = -135 + scrolled + "%";
-  })
+  document.querySelectorAll(".transition:nth-child(even)").forEach((e) => { e.style.left = -135 + scrolled + "%"; })
+  document.querySelectorAll(".transition:nth-child(odd)").forEach((e) => { e.style.right = -135 + scrolled + "%"; })
 }
 
 function typeName() {
   const h1 = document.querySelector('.App-header h1');
   const h2 = document.querySelector('.App-header h2');
 
+  const name = 'Lukas Okkenhaug';
+  const desc = 'A Creative Fella from Norway';
+
   h1.innerHTML = '';
   h2.innerHTML = '';
   function nameAppear() {
+    for (let i = 0; i < name.length; i++)
+      setTimeout(() => { h1.innerHTML += name[i, i]; }, i * 125);
 
-  const cursor = document.createElement('span');
-  cursor.classList.add('cursor');
-  cursor.innerHTML = '|';
-
-  for (let i = 0; i < user.name.length; i++) {
-    setTimeout(() => {
-      const name = document.createElement('span');
-      name.innerHTML = user.name[i, i];
-      h1.append(name);
-      h1.append(cursor);
-    }, i * 125);
-  }
-  for (let i = 0; i < user.desc.length; i++) {
-    setTimeout(() => {
-      const desc = document.createElement('span');
-      desc.innerHTML = user.desc[i, i];
-      h2.append(desc);
-      h2.append(cursor);
-    }, (user.name.length * 150) + (i * 75));
-  }
+    for (let i = 0; i < desc.length; i++)
+      setTimeout(() => { h2.innerHTML += desc[i, i]; }, (name.length * 150) + (i * 75));
   } nameAppear();
 }
 
 window.onload = () => {
-  window.onscroll = function() {scroll()};
+  window.onscroll = function() { scroll() };
   document.querySelector('.load').remove();
   document.querySelector("nav a").addEventListener("click", changebackgrund);
-  document.querySelector("#age").innerHTML = user.age;
+  document.querySelector("#age").innerHTML = `I am ${(d.getFullYear() - (d.getMonth() <= 7 ? 2007 : 2006))} Years old`;
+  
   changebackgrund();
-
-  let sentence = document.querySelector(".App-header section .line2 p").innerHTML;
-  for (let i = 0; i < 100; i++) {
-      document.querySelectorAll(".App-header section .line p").forEach((e) => {e.innerHTML += sentence})
-  }
-
-  let header = Math.floor(Math.random() * document.querySelectorAll(".App-header section").length + 2);
-  document.querySelector(`.App-header section:nth-child(${header})`).style.display = "block";
-  if (header === 3) typeName()
+  typeName();
 
   document.querySelectorAll('.icons-container img').forEach((e) => {
     e.addEventListener('click', () => {
